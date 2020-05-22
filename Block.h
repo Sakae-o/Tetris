cfa700559afdd93bdc37251ea88133705959ca46
盛羽 <60038013+Taketo-i@users.noreply.github.com>
2020-05-22 12:19:18 +0800

    Update Block.h

--- Block.h
+++ Block.h
@@ -1,7 +1,11 @@
+#pragma once
+
+
 class Block {
 public:
 	void InitialBlock();	//初始化方块
-	void DrawBlock();		//打印方块
+	void DrawBlock(int map_x);		//绘制方块
+	void clearBlock(int max_p);		//消除方块
 	void DrewNext();		//在游戏区域旁打印下一个方块
 
 	bool JudgeLeft();		//分别判断是否可以左移，右移，下落
@@ -16,9 +20,12 @@ public:
 	void Down();		//自动下落
 	void accelerate();	//手动加速
 
-	friend void fuse(Block now, Map A);
-
 private:
-	int shape[4][4];	//形状
-	int x, y;	//位置
+	int shape[4][4];
+	int x, y;		//这里的x，y表示中心的地图坐标
+	int red, green, blue;	//颜色数据
+
+	int type;	//方块种类
+	int state;	//方块当前的旋转方向
 };
+
