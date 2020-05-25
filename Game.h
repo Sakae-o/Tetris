24578dea999304671a5049c93a8a7e6fa83bef7f
盛羽 <60038013+Taketo-i@users.noreply.github.com>
2020-05-25 17:46:27 +0800

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,11 +1,13 @@
+#pragma once
+
 class Game {
 public:
-	void Welcome();		//负责介绍规则和操作，选择开始或退出
-	void GameOver();	//显示胜方，选择重来或退出
-	void GameStop();	//暂停游戏
+	static void Welcome();		//负责介绍规则和操作，选择开始或退出
+	static void GameOver();	//显示胜方，选择重来或退出
+	static void GameStop();	//暂停游戏
 
-	void KeyControl();		//处理键盘输入
+	static void KeyControl();		//处理键盘输入
 
-	void HideCursor();	 //隐藏光标
 
+	static bool isStop;		//判断是否处于暂停
 };
