7e0199219dafaec11060a8d7075f1372f97ed22b
盛羽 <60038013+Taketo-i@users.noreply.github.com>
2020-05-31 13:11:53 +0800

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -5,6 +5,8 @@
 #include<graphics.h>
 #include<conio.h>
 #include"Map.h"
+#include"Block.h"
+
 #define MAXY 24
 #define MAXX 15
 #define LEN 18
@@ -38,4 +40,86 @@ void Map::DrawMap() {
 		}
 	}
 }
+
+void Map::clearMap() {
+	setfillcolor(BLACK);
+	for (int i = 0; i < MAXY; i++) {
+		for (int j = 0; j < MAXX; j++) {
+			int xx = x + j * (LEN + 3);		//这里的+3是预留了每个小方块之间的间距
+			int yy = y + i * (LEN + 3);
+			solidrectangle(xx, yy, xx + LEN, yy + LEN);
+		}
+	}
+}
+
+int Map::Clear_Line() {
+	int cnt = 0;
+	for (int i = 1; i < MAXY - 1; i++) {
+		int flag = 1;
+		for (int j = 1; j < MAXX - 1; j++) {
+			if (map[i][j] == 0) {
+				flag = 0;
+				break;
+			}
+		}
+
+		if (flag == 1) {
+			for (int yy = i; yy > 0; yy--) {
+				for (int j = 1; j < MAXX - 1; j++) {
+					map[yy][j] = map[yy - 1][j];
+				}
+			}
+			cnt++;
+		}
+	}
+	return cnt;
+}
+
+void Map::Add_line(int cnt) {
+	int top;					//寻找当前最高层所在的y坐标top
+	for (top = 0; top < MAXY - 1; top++) {
+		for (int j = 1; j < MAXX - 1; j++) {
+			if (map[top][j] == 1) {
+				goto part2;
+			}
+		}
+	}
+
+part2:
+	while (cnt-- && top > 0) {
+		for (int i = top - 1; i < MAXY - 2; i++) {
+			for (int j = 1; j < MAXX - 1; j++) {
+				map[i][j] = map[i + 1][j];
+			}
+		}
+		top--;
+
+		int flag = 0;		//用于判断，以防出现生成空行的情况
+		int j = 2;
+		while (!flag) {
+			for (int i = 1; i < MAXX - 1; i++) {
+				map[MAXY - j][i] = rand() % 2;
+				if (map[MAXY - j][i]) {
+					flag = 1;
+				}
+			}
+		}
+		j++;
+
+	}
+}
+
+
+int Map::Clear_update() {
+	clearMap();
+	int cnt = Clear_Line();
+	DrawMap();
+	return cnt;
+}
+void Map::Add_update(int cnt) {
+	clearMap();
+	Add_line(cnt);
+	DrawMap();
+}
+
 
