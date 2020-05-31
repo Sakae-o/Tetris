9afb203b41a8eee301db4ea39f7e79ec16ce5a84
盛羽 <60038013+Taketo-i@users.noreply.github.com>
2020-05-31 13:12:22 +0800

    Update Map.h

--- Map.h
+++ Map.h
@@ -1,21 +1,29 @@
 #pragma once
 
 #include"Block.h"
+#include"Map.h"
 
 const int MAXY = 24;
 const int MAXX = 15;
+
+class Block;
 
 class Map {
 public:
 	Map(int xx, int yy);
 
 	void DrawMap();		//显示游戏区域
+	void clearMap();
 
-	bool JudgeClear();	//判断是否可消行
-	void Clear_Line();	//执行消行
-	void Add_line();	//同时对方加一行
+	int Clear_Line();	//判断并执行消行，返回的是消的行数
+	void Add_line(int cnt);	//同时对方加cnt行，调用时cnt为对方方块的Clear_Line的值
 
+	int Clear_update();		//消行后更新地图,返回的是消的行数
+	void Add_update(int cnt);			//增行后更新地图
+
+	friend void fuse(Map& M, Block& B);
 
 	int map[MAXY][MAXX];	//地图数据
 	int x, y;		//左上角的像素坐标
 };
+
