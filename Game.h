39b89115a4c3747f64e1ad3a66aae2066cb62109
盛羽 <60038013+Taketo-i@users.noreply.github.com>
2020-06-02 18:26:55 +0800

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,13 +1,11 @@
 #pragma once
+#include"Map.h"
+#include<string>
 
 class Game {
 public:
-	static void Welcome();		//负责介绍规则和操作，选择开始或退出
-	static void GameOver();	//显示胜方，选择重来或退出
+	static bool Welcome();		//负责介绍规则和操作，选择开始或退出
+	static bool GameOver(Map P1_map);	//显示胜方，选择重来或退出
 	static void GameStop();	//暂停游戏
-
-	static void KeyControl();		//处理键盘输入
-
-
-	static bool isStop;		//判断是否处于暂停
+	static void clearScreen(); //屏幕清为全黑
 };
